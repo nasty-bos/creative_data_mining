368d391c65d0b88665540c3daa534a04e9354aec
Emanuel Riegelbauer <remanuel@ethz.ch>
2018-04-18 23:01:51 +0200

    clean up

--- delay.py
+++ delay.py
@@ -1,36 +1,19 @@
-import sqlite3
-import glob
-import io
 import requests
 import pandas as pd
-import csv
-import sys
 
-if sys.version_info[0] == 3:
-    from urllib.request import urlopen
-else:
-    from urllib import urlopen
+
+def mask(df, key, value):
+    return df[df[key] == value]
+
 
 url = 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/03ec9d0a-b16f-4e78-8e4f-2da4970efbb6/download/fahrzeiten_soll_ist_20180325_20180331.csv'
 s = requests.get(url).content
 c = pd.read_csv(io.StringIO(s.decode('utf-8')))
-print(c)
+print(c.head())
 
+df = pd.DataFrame(data=c)
+df.drop(df.columns[[range(3, 10), range(13, 33)]], axis=1, inplace=True)
+print(df.head())
 
-conn = sqlite3.connect('c.db')
-cur = conn.cursor()
-# ss.to_sql(delay, conn, if_exists='append', index=False)
-
-cmd = "CREATE TABLE IF NOT EXISTS %s (=)"
-
-cur.execute("select * from linie")  # use your column names here
-#
-# with open('s', 'rb') as fin:  # `with` statement available in 2.5+
-#     # csv.DictReader uses first line in file for column headings by default
-#     dr = csv.DictReader(fin)  # comma is default delimiter
-#     to_db = [(i[''], i['col2']) for i in dr]
-#
-# cur.executemany("INSERT INTO t (col1, col2) VALUES (?, ?);", to_db)
-# con.commit()
-# con.close()
-# db.close()
+pd.DataFrame.mask = mask
+df.mask(df, 'linie', 69)
