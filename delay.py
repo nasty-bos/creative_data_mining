8edc54ce95cb507c3b8743dab72f7d47b02a0923
Emanuel Riegelbauer <remanuel@ethz.ch>
2018-04-23 14:00:38 +0200

    clean py

--- delay.py
+++ delay.py
@@ -1,27 +1,31 @@
-import io
 import shutil
-import requests
 import pandas as pd
-import numpy as np
-import csv
 import glob
 import os
-from sklearn import linear_model
-from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+from scipy import stats
+from sklearn.svm import SVC
 
 
-def mask(df, key, value):
-    return df[df[key] == value]
+def mask(df3, key, value):
+    return df3[df3[key] == value]
 
 
 # turn off warnings
 pd.options.mode.chained_assignment = None
 
 ## Import of delay data
-
-# 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/03ec9d0a-b16f-4e78-8e4f-2da4970efbb6/download/fahrzeiten_soll_ist_20180325_20180331.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/c88a3801-c6fc-4d32-8ece-e269899be497/download/fahrzeiten_soll_ist_20180318_20180324.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/eb403fd1-8f8b-475e-98aa-f04ee3b255ba/download/fahrzeiten_soll_ist_20180311_20180317.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/1ac13127-fcde-4ac2-8462-50f348fd28fe/download/fahrzeiten_soll_ist_20180218_20180224.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/97e59d2a-83ec-438f-ae6f-0fe85d9bc1e6/download/fahrzeiten_soll_ist_20180304_20180310.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/b45b383e-4b0d-4ad0-8bee-e958c5e7360a/download/fahrzeiten_soll_ist_20180121_20180127.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/f17a950d-5be5-4b00-bafd-3c859afcc6cc/download/fahrzeiten_soll_ist_20180204_20180210.csv', 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/a38c5d0f-b732-4f5a-9786-eb01a2ffa0bb/download/fahrzeiten_soll_ist_20180211_20180217.csv', 
 listpaths = [
-    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/a265b5d8-287f-4d22-88b2-f3a1770e1a4a/download/fahrzeiten_soll_ist_20180225_20180303.csv']
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/a265b5d8-287f-4d22-88b2-f3a1770e1a4a/download/fahrzeiten_soll_ist_20180225_20180303.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/03ec9d0a-b16f-4e78-8e4f-2da4970efbb6/download/fahrzeiten_soll_ist_20180325_20180331.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/c88a3801-c6fc-4d32-8ece-e269899be497/download/fahrzeiten_soll_ist_20180318_20180324.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/eb403fd1-8f8b-475e-98aa-f04ee3b255ba/download/fahrzeiten_soll_ist_20180311_20180317.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/1ac13127-fcde-4ac2-8462-50f348fd28fe/download/fahrzeiten_soll_ist_20180218_20180224.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/97e59d2a-83ec-438f-ae6f-0fe85d9bc1e6/download/fahrzeiten_soll_ist_20180304_20180310.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/b45b383e-4b0d-4ad0-8bee-e958c5e7360a/download/fahrzeiten_soll_ist_20180121_20180127.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/f17a950d-5be5-4b00-bafd-3c859afcc6cc/download/fahrzeiten_soll_ist_20180204_20180210.csv',
+    'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/resource/a38c5d0f-b732-4f5a-9786-eb01a2ffa0bb/download/fahrzeiten_soll_ist_20180211_20180217.csv']
 frame = pd.DataFrame()
 list_ = []
 for path_ in listpaths:
@@ -33,14 +37,9 @@ check = pd.DataFrame(data=df)
 col = check.shape[1]
 row = check.shape[0]
 
-print(row)
-print(col)
-# print(df)
-print(df.head(2))
-# for i in range(2):
-#     s = requests.get(path.i).content
-#     c = pd.read_csv(glob.glob(io.StringIO(s.decode('utf-8')[0]), index_col=None)
-#     df = pd.DataFrame(data=c)
+print('delay data rows: %d' % row)
+print('delay data columns: %d' % col)
+# print(df.head(2))
 
 ## Time difference calculation and formatting
 
@@ -49,9 +48,6 @@ df.drop(df.columns[
              33]], axis=1, inplace=True)
 pd.DataFrame.mask = mask
 df2 = df.mask('linie', 69)
-check = pd.DataFrame(data=df)
-col = check.shape[1]
-row = check.shape[0]
 
 df2.loc[:, 'diff'] = (df2['ist_an_von'] - df2['soll_an_von'])
 dp = df2.copy()
@@ -67,12 +63,16 @@ df1.loc[:, 'diff'] = df1.loc[:, 'diff'].apply(pd.to_numeric, errors='coerce', do
 df1.loc[:, 'time'] = df1.loc[:, 'time'].dt.round('60min')
 df1 = df1.dropna(how='any')
 df1['diff'] = df1['diff'].astype(int)
-df1 = df1.loc[df1['diff'] > 60]
+# df1 = df1.loc[df1['diff'] > 60]
 
-print(row)
-print(col)
-print(df1.head())
-print(df1.dtypes)
+check = pd.DataFrame(data=df1)
+col = check.shape[1]
+row = check.shape[0]
+
+print('cleaned diff data rows: %d' % row)
+print('cleaned diff data columns: %d' % col)
+# print(df1.head())
+# print(df1.dtypes)
 
 ## Weather Data
 
@@ -96,40 +96,21 @@ wet = wet.dropna(how='any')
 
 q = wet.shape[1]
 o = wet.shape[0]
-print('Number of columns: %d' % q)
-print('Number of rows: %d' % o)
-print(wet.head())
-print(wet.dtypes)
+print('weather data rows: %d' % o)
+print('weather data columns: %d' % q)
+# print(wet.head())
+# print(wet.dtypes)
 
 ## Merge
-
-# def nearest(items, pivot):
-#     return min(items, key=lambda x: abs(x - pivot))
-# df1.apply(nearest(wet.loc[:,'time'],df1.loc[:,'time']))
-# df1.set_index(df1.loc[:,'time'], inplace=True)
-# wet.set_index(wet.loc[:,'time'], inplace=True)
-
-# print(df1.head())
-# print(wet.head())
-
-# left merge on both time col which are in datetime format
+# merge on both time col which are in datetime format
 merge = df1.merge(wet, left_on='time', right_on='time', how='inner')
 
-print(merge.head(3))
-print(merge.dtypes)
-# df1.iloc[df1.index.get_loc(wet.loc[:,'time'],method='nearest')]
-# df1['time'] = pd.DatetimeIndex(wet['time']).normalize()
-# df1.apply(lambda col: col.drop_duplicates().reset_index(drop=True))
-# pred = pd.merge(df1, wet).head()
+# print(merge.head(3))
+# print(merge.dtypes)
 
 ## Correlation
 
-import matplotlib.pyplot as plt
-from matplotlib import style
-
 style.use("ggplot")
-from sklearn import svm
-from scipy import stats
 
 x = merge.loc[:, 'rain']
 y = merge.loc[:, 'diff']
@@ -143,20 +124,17 @@ plt.scatter(x, y)
 plt.title("Correlation")
 plt.xlabel("rain[mm/h]")
 plt.ylabel("delay[s]")
-plt.savefig('./output/plot.png', format='png', dpi=1200)
+plt.savefig('./output/correlation.png', format='png', dpi=800)
 plt.show()
 
-print(merge.loc[:, 'rain'].describe())
-print(merge.loc[:, 'diff'].describe())
-
+# print(merge.loc[:,'rain'].describe())
+# print(merge.loc[:,'diff'].describe())
 slope, intercept, r_value, p_value, std_err = stats.linregress(merge.loc[:, 'rain'], merge.loc[:, 'diff'])
 print("r-squared:", r_value ** 2)
 
 ## Prediction
-
-X = merge.loc[:, ['rain']]
+X = merge.loc[:, 'rain']
 y = merge.loc[:, 'diff']
-from sklearn.svm import SVC
 
 clf = SVC()
 clf.fit(X, y)
@@ -170,14 +148,3 @@ plt.ylabel('predicted delay [s]')
 plt.xlabel('precipitation [mm/h]')
 plt.savefig('./output/prediction.png', format='png', dpi=800)
 plt.show()
-
-# x_train = merge.loc[:,'rain']
-# y_train = merge.loc[:,'diff']
-# x_test = merge.loc[:,'rain']
-
-# linear = linear_model.LinearRegression()
-# linear.fit(x_train, y_train)
-# linear.score(x_train, y_train)
-# print('Coefficient: \n', linear.coef_)
-# print('Intercept: \n', linear.intercept_)
-# predicted= linear.predict(x_test)
